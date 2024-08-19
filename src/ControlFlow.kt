8d5159b9b9d07ba44a780e093486a563255bf92a
rikoadii <radi60372@gmail.com>
2024-08-19 22:16:00 +0800

    complete control-flow

--- src/ControlFlow.kt
+++ src/ControlFlow.kt
@@ -1,6 +1,8 @@
 fun main() {
     val value: Any = 10
-    val ranges = 6..10
+//    step buat ngatur, gunakan downTo jika nilai turun, terdapat IntRange, LongRange, CharRange
+    val ranges = 1..10 step 2
+    var counter = 4
 //    when == switch case
 //    bisa dijadikan expression
     val isiValue = when (value) {
@@ -20,4 +22,29 @@ fun main() {
         else -> "tidak valid"
     }
     println(isiValue)
+
+//    while & do while, umum
+    do {
+        println("p")
+        counter--
+    } while (
+        counter > 1
+    )
+
+//    continue & break dengan label @
+    outerLoop@ for (i in 1..4){
+        for (j in 1..4){
+//            jadi, outerLoop akan berhenti
+            if (j == 2) break@outerLoop
+            println("i: $i, j: $j")
+        }
+    }
+
+    outerLoop2@ for (i in 1..4){
+        for (j in 1..4){
+//            jadi, loop kedua akan dilompati
+            if (j == 2) continue@outerLoop2
+            println("i: $i, j: $j")
+        }
+    }
 }
